57829e1bea18f5c0795b6b01997a6069fbdddf47
Noa Aluk <noaluck@walla.com>
2021-03-16 20:31:49 +0200

    8

--- equations.py
+++ equations.py
@@ -1,12 +1,16 @@
- 
 
 def ln(x:float):
-    yn=0
-    i=0
-    while i<=100:
+    yn=x-1.0
+    yn1=0
+    a=yn-yn1
+    if a<0:
+        a=a*(-1)
+    while a>0.001:
         yn1=yn+2*((x-exponent(yn))/(x+exponent(yn)))
+        a=yn-yn1
+        if a<0:
+            a=(-1)*a
         yn=yn1
-        i+=1
     return yn1
 
 
@@ -17,10 +21,8 @@ def exponent(x:float):
         b=1
         exp1=0
         while i<=100:
-            #mehane
             z=i
             h*=z
-            #mone
             a=x
             b=b*a
             exp=b/h
@@ -29,7 +31,6 @@ def exponent(x:float):
         return exp1+1
     else:
         return 1
-
 
 def XtimesY(x:float,y:float):
     if x>0:
@@ -41,37 +42,16 @@ def XtimesY(x:float,y:float):
         return 0
     elif x<0 and y==0:
         return 1
-    else: 
-        if y>0:
-            i=1
-            z1=1
-            while i<=y:
-                z=z1*x
-                z1=z
-                i+=1
-            return z1
-        if y<0:
-            i=1
-            z1=1
-            while i<=(-y):
-                z=z1*x
-                z1=z
-                i+=1
-            return 1/z1
-
-
+    else:
+        return 0
   
+
 def sqrt(x:float,y:float):
     if y>=0 and x!=0:
        return XtimesY(y, (1/x))
-    elif y<0 or x==0:
+    else:
         return 0
-    elif y==0 and x==0:
-        return 0
-
-
 
 
 def calculate(x:float):
     return exponent(x)*XtimesY(7,x)*XtimesY(x,-1)*sqrt(x,x)
-
