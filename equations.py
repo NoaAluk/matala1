76413cd7f347588dbad00858ef0d2a2185269048
Noa Aluk <noaluck@walla.com>
2021-03-16 20:35:45 +0200

    9

--- equations.py
+++ equations.py
@@ -42,8 +42,23 @@ def XtimesY(x:float,y:float):
         return 0
     elif x<0 and y==0:
         return 1
-    else:
-        return 0
+    else: 
+        if y>0:
+            i=1
+            z1=1
+            while i<=y:
+                z=z1*x
+                z1=z
+                i+=1
+            return z1
+        if y<0:
+            i=1
+            z1=1
+            while i<=(-y):
+                z=z1*x
+                z1=z
+                i+=1
+            return 1/z1
   
 
 def sqrt(x:float,y:float):
